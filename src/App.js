20ea5fdf1c0700e79f0fd5b77f2506a228809ad5
Sudhansu Sekhar Sahoo <sudhansusahoo359@gmail.com>
2024-05-01 13:20:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import { ReactToPrint } from 'react-to-print';
 
 function App() {
+  const componentRef = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactToPrint
+        trigger={() => {
+          return <button>Print the table</button>;
+        }}
+        content={() => componentRef.current}
+      />
+      <div ref={componentRef}>
+        <p style={{fontSize:'10px', margin:'0px'}}>Hello</p>
+        <p style={{fontSize:'50px', margin:'0px'}}>Hello</p>
+        <p style={{fontSize:'10px', margin:'0px'}}>Hello</p>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Month</th>
+              <th>Product A</th>
+              <th>Product B</th>
+              <th>Total Sales</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>January</td>
+              <td>100</td>
+              <td>150</td>
+              <td>250</td>
+            </tr>
+            <tr>
+              <td>February</td>
+              <td>120</td>
+              <td>130</td>
+              <td>250</td>
+            </tr>
+            <tr>
+              <td>March</td>
+              <td>140</td>
+              <td>110</td>
+              <td>250</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>Total</td>
+              <td>360</td>
+              <td>390</td>
+              <td>750</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 }
